fe4305c9148a5973685952d93351ccdea92ff874
Henrique Tavares <ihenrits@gmail.com>
2019-05-23 23:36:17 -0300

    Definindo rota inicial

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+// import AsyncStorage from '@react-native-community/async-storage';
 
 // import { Container } from './styles';
 
-const Repositories = () => <View />;
+// AsyncStorage.clear();
+
+const Repositories = () => (
+  <View>
+    <Text>Repositórios</Text>
+  </View>
+);
 
 export default Repositories;
