70f967300c2a2ba486d02c81e98c01f29ab4c148
Henrique Tavares <ihenrits@gmail.com>
2019-05-24 22:09:51 -0300

    Estilizando abas

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Organizations = () => <View />;
+import Header from '~/components/Header';
+
+const Organizations = () => (
+  <View>
+    <Header title="Organizações" />
+  </View>
+);
+
+const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Organizations.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Organizations;
